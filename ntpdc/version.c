ab30a680c113237ddd5200a70d025a9e35ad3fd7
bipulkumar7 <bipul.opensource@gmail.com>
2021-03-01 01:42:58 -0500

    custom ntp

---
+++ ntpdc/version.c
@@ -0,0 +1,5 @@
+/*
+ * version file for ntpdc
+ */
+#include <config.h>
+const char * Version = "ntpdc 4.2.8p15@1.3728 Sun Feb 28 16:20:05 UTC 2021 (1)";
---
+++ util/version.c
@@ -0,0 +1,5 @@
+/*
+ * version file for ntp-keygen
+ */
+#include <config.h>
+const char * Version = "ntp-keygen 4.2.8p15@1.3728 Sun Feb 28 16:20:15 UTC 2021 (1)";
